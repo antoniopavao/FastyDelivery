379059105e36d8c501e1215a64db0347b5966f88
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-08-01 10:47:57 -0300

    Creating authenticate module

--- src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
+++
@@ -1,8 +0,0 @@
-export class AuthenticateClientUseCase {
-  async execute() {
-    //  Receive username and password
-    // Verufy if user exists
-    // Verify if the password is valid
-    // Generate token
-  }
-}
---
+++ src/modules/account/useCases/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,44 @@
+import { prisma } from '../../../../database/prismaClient';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+declare var process: {
+  env: {
+    JSON_SECRET_KEY: string;
+  };
+};
+
+interface IAuthenticateClient {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateClientUseCase {
+  async execute({ username, password }: IAuthenticateClient) {
+    //  Receive username and password
+
+    // Verify if user exists
+    const client = await prisma.clients.findFirst({
+      where: { username: username },
+    });
+
+    if (!client) {
+      throw new Error(`Username or password invalid!`);
+    }
+
+    // Verify if the password is valid
+    const passwordMatch = await compare(password, client.password);
+
+    if (!passwordMatch) {
+      throw new Error('Username or password invalid!');
+    }
+
+    // Generate token
+    const token = sign({ username }, process.env.JSON_SECRET_KEY, {
+      subject: client.id,
+      expiresIn: '1d',
+    });
+
+    return token;
+  }
+}
