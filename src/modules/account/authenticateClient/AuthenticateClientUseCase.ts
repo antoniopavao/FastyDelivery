99416298bc2604a7d7b694678f742baee8c721c4
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-08-01 09:49:56 -0300

    Creating authenticateClient module

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,8 @@
+export class AuthenticateClientUseCase {
+  async execute() {
+    //  Receive username and password
+    // Verufy if user exists
+    // Verify if the password is valid
+    // Generate token
+  }
+}
