3b7670c13cf73b7e31c69617d15f36582c3388d3
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-08-07 03:13:23 -0300

    updating method to return only the deliveries

--- src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
+++ src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
@@ -4,7 +4,7 @@ export class FindAllDeliveriesUseCase {
   async execute(client_id: string) {
     const deliveries = await prisma.clients.findMany({
       where: { id: client_id },
-      include: { Deliveries: true },
+      select: { Deliveries: true },
     });
 
     return deliveries;
