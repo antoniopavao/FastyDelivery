951e35dcf6e22a8eb88e638b540d124fd28f7ace
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-08-07 11:38:56 -0300

    fixing insensitive case error

--- src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
+++ src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
@@ -4,7 +4,7 @@ export class FindAllDeliveriesUseCase {
   async execute(client_id: string) {
     const deliveries = await prisma.clients.findMany({
       where: { id: client_id },
-      select: { Deliveries: true },
+      select: { Deliveries: true, id: true, username: true },
     });
 
     return deliveries;
