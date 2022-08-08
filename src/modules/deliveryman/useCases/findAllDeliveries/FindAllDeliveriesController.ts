bdcaa9e18bb2290056b1ecc48fc0b62f1cb41db3
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-08-08 11:29:47 -0300

    creating findAllDeliveries - deliveryman

---
+++ src/modules/deliveryman/useCases/findAllDeliveries/FindAllDeliveriesController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { FindAllDeliveriesUseCase } from './FindAllDeliveriesUseCase';
+
+export class FindAllDeliveriesController {
+  async handle(request: Request, response: Response) {
+    const { deliveryman_id } = request;
+    const findAllDeliveriesUseCase = new FindAllDeliveriesUseCase();
+    const deliveries = await findAllDeliveriesUseCase.execute(deliveryman_id);
+    return response.json(deliveries);
+  }
+}
